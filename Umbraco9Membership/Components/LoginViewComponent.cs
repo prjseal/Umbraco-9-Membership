18b1239ff14f5400b8e6d4fb2a3ad005b48171ba
Paul Seal <prjseal@gmail.com>
2021-09-30 00:12:03 +0100

    got login form working

---
+++ Umbraco9Membership/Components/LoginViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco9Membership.Models.ViewModels;
+
+namespace Umbraco9Membership.Components
+{
+    [ViewComponent(Name = "Login")]
+    public class LoginViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(LoginViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Umbraco9Membership/Controllers/Surface/AccountSurfaceController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Common.Security;
+using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco9Membership.Models.ViewModels;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace Umbraco9Membership.Controllers.Surface
+{
+    public class AccountSurfaceController : SurfaceController
+    {
+        private readonly IMemberSignInManager signInManager;
+
+        public AccountSurfaceController(IMemberSignInManager signInManager, IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext services, AppCaches appCaches,
+            IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+            this.signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+
+            SignInResult result = await signInManager.PasswordSignInAsync(
+                model.Username, model.Password, isPersistent: model.RememberMe, lockoutOnFailure: true);
+
+            return RedirectToCurrentUmbracoUrl();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToCurrentUmbracoUrl();
+        }
+    }
+}
